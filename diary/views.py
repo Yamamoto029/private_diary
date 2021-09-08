c3143869f65262d29f6828bdf68a6177e208a970
Yamamoto029 <ksw2070290@stu.o-hara.ac.jp>
2021-09-08 12:23:43 +0900

    フォーム機能作成（前半）

--- diary/views.py
+++ diary/views.py
@@ -1,9 +1,14 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+import logging
+from django.urls import reverse_lazy
+
 from django.views import generic
 
 from .forms import InquiryForm
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
@@ -15,3 +20,9 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
     template_name = "diary/inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        logger.info('Inquiry send by {}'.format(form.cleaned_data['name']))
+        return super().from_valid(form)
