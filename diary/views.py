349a9f88fd853351203e82fae3ea2af4527b8014
Yamamoto029 <ksw2070290@stu.o-hara.ac.jp>
2021-09-14 12:31:44 +0900

    chapter11 10

--- diary/views.py
+++ diary/views.py
@@ -8,7 +8,7 @@ from django.urls import reverse_lazy
 
 from django.views import generic
 
-from .forms import InquiryForm
+from .forms import InquiryForm, DiaryCreateForm
 
 logger = logging.getLogger(__name__)
 
@@ -42,7 +42,24 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
         diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
         return diaries
 
-class DiaryDetailView(LoginRequiredMixin, generic.DateDetailView):
+class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
     model = Diary
     template_name = 'diary_detail.html'
-    pk_url_kwarg = 'id'
+
+
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request,'日記を作成しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request,"日記の作成に失敗しました。")
+        return super().form_invalid(form)
